ee26e4b3907746a81a250b1a6d8c51be32af53f1
Pi-et-Rho <pierre.michel.work@gmail.com>
2024-03-21 16:32:40 +0100

    Add different spawn methode

--- Food.py
+++ Food.py
@@ -11,6 +11,8 @@ class Food:
         self.color = color
         self.food_positions = []
 
+
+    # Draw random
     def spawn_food(self, food_count):
         self.food_positions = []
 
@@ -24,7 +26,36 @@ class Food:
         y = random.randint(0, self.grid_height - 1)
         self.food_positions.append((x, y))
 
-    def draw_food(self, screen):
+    # Draw lines
+    def spawn_food_in_lines(self, num_lines, grid_width, grid_height):
+        self.food_positions = []
+ 
+        centre_x = grid_width // (num_lines + 1)
+        centre_y = grid_height // (num_lines + 1)
+ 
+        for i in range(1, num_lines + 1):
+            y = (i * centre_y) - 1
+            for x in range(grid_width):
+                self.food_positions.append((x, y))
+ 
+        for i in range(1, num_lines + 1):
+            x = (i * centre_x) - 1
+            for y in range(grid_height):
+                self.food_positions.append((x, y))
+
+    # Draw rectangle
+    def spawn_food_rectangle(self, width, height, grid_width, grid_height):
+        start_x = (grid_width - width) // 2
+        start_y = (grid_height - height) // 2
+
+        self.food_positions = []
+
+        for x in range(start_x, start_x + width):
+            for y in range(start_y, start_y + height):
+                self.food_positions.append((x, y))
+
+
+    def draw_food(self, screen,):
         for position in self.food_positions:
             x, y = position
             pygame.draw.rect(screen, self.color, (x * self.tilesize, y * self.tilesize, self.tilesize, self.tilesize))
--- Simulator.py
+++ Simulator.py
@@ -14,44 +14,99 @@ pygame.display.set_caption("Life Simulator")
 grid = Grid(X_GRID, Y_GRID, TILESIZE)
 grid.set_color(GRID_COLOR)
 
-# Food
-food = Food(X_GRID, Y_GRID, TILESIZE, FOOD_COLOR)
-food.spawn_food(FOOD_INITIAL_COUNT)
+# Food Random
+food_random = Food(X_GRID, Y_GRID, TILESIZE, FOOD_COLOR)
+food_random.spawn_food(FOOD_INITIAL_COUNT)
+# Food Lines
+food_lines = Food(X_GRID, Y_GRID, TILESIZE, FOOD_COLOR)
+food_lines.spawn_food_in_lines(LINES_NUMBER, X_GRID, Y_GRID)
+# Food Rectangle
+food_rectangle = Food(X_GRID, Y_GRID, TILESIZE, FOOD_COLOR)
+food_rectangle.spawn_food_rectangle(200, 200, X_GRID, Y_GRID)
 
 # Microbe
 microbes = [Microbes(random.randint(0, X_GRID - 1), random.randint(0, Y_GRID - 1)) for _ in range(MICROBE_COUNT)]
 
+current_module = "draw random"
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                current_module = "draw random"
+            elif event.key == pygame.K_b:
+                current_module = "draw lines"
+            elif event.key == pygame.K_c:
+                current_module = "draw rectangle"
 
     screen.fill(SCREEN_COLOR)
 
     # Grid
     grid.draw_grid(screen)
 
-    # Food
-    food.draw_food(screen)
-    for _ in range(FOOD_SPAWN_RATE):
-        food.draw_single_food()
+    # Different module (change food generation)
+    # Random
+    if current_module == "draw random":
+        # Food
+        food_random.draw_food(screen)
+        for _ in range(FOOD_SPAWN_RATE):
+            food_random.draw_single_food()
 
-    # Microbe
-    new_microbes = []
-    for microbe in microbes:
-        microbe.move()
-        microbe.evolve()
-        if (microbe.x, microbe.y) in food.food_positions:
-            if microbe.eat_food(food):
-                new_microbes.append(microbe.reproduce())
-        microbe.draw_microbes(screen)
+        # Microbe
+        new_microbes = []
+        for microbe in microbes:
+            microbe.move()
+            microbe.evolve()
+            if (microbe.x, microbe.y) in food_random.food_positions:
+                if microbe.eat_food(food_random):
+                    new_microbes.append(microbe.reproduce())
+            microbe.draw_microbes(screen)
+            if microbe.energy <= 0:
+                microbes.remove(microbe)
+        microbes.extend(new_microbes)
 
-        if microbe.energy <= 0:
-            microbes.remove(microbe)
+    # Lines
+    elif current_module == "draw lines":
+        # Food
+        food_lines.draw_food(screen)
+        for _ in range(FOOD_SPAWN_RATE_LINES):
+            food_lines.draw_single_food()
+        
+        # Microbe
+        new_microbes = []
+        for microbe in microbes:
+            microbe.move()
+            microbe.evolve()
+            if (microbe.x, microbe.y) in food_lines.food_positions:
+                if microbe.eat_food(food_lines):
+                    new_microbes.append(microbe.reproduce())
+            microbe.draw_microbes(screen)
+            if microbe.energy <= 0:
+                microbes.remove(microbe)
+        microbes.extend(new_microbes)
+    
+    # Rectangle
+    elif current_module == "draw rectangle":
+        # Food
+        food_rectangle.draw_food(screen)
+        for _ in range(FOOD_SPAWN_RATE_RECTANGLE):
+            food_rectangle.draw_single_food()
 
-    microbes.extend(new_microbes)
+        # Microbe
+        new_microbes = []
+        for microbe in microbes:
+            microbe.move()
+            microbe.evolve()
+            if (microbe.x, microbe.y) in food_rectangle.food_positions:
+                if microbe.eat_food(food_rectangle):
+                    new_microbes.append(microbe.reproduce())
+            microbe.draw_microbes(screen)
+            if microbe.energy <= 0:
+                microbes.remove(microbe)
+        microbes.extend(new_microbes)
 
     pygame.display.flip()
 
